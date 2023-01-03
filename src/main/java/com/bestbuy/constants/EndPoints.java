80c14d295baca015911b584b372a824c5fd0c5d7
kajalkhunt <kajaljkhunt@gmail.com>
2023-01-03 22:18:42 +0000

    p1 bestbuy serenity wk18 kajalk

---
+++ src/main/java/com/bestbuy/constants/EndPoints.java
@@ -0,0 +1,30 @@
+package com.bestbuy.constants;
+
+/**
+ * Created by Jay
+ */
+public class EndPoints {
+
+    /**
+     * This is Endpoints of products
+     */
+    public static final String GET_SINGLE_PRODUCT_BY_ID = "/products/{productID}";
+    public static final String GET_ALL_PRODUCTS = "/products";
+    public static final String UPDATE_PRODUCT_BY_ID = "/products/{productID}";
+    public static final String DELETE_PRODUCT_BY_ID = "/products/{productID}";
+
+    /**
+     * This is Endpoints of stores
+     */
+    public static final String GET_SINGLE_STORE_BY_ID = "/stores/{storeID}";
+    public static final String UPDATE_STORE_BY_ID = "/stores/{storeID}";
+    public static final String DELETE_STORE_BY_ID = "/stores/{storeID}";
+
+
+
+    /**
+     * This is Endpoints of Authentication api
+     */
+
+
+}
---
+++ src/test/java/com/bestbuy/testsuite/ProductCURDWithSteps.java
@@ -0,0 +1,73 @@
+package com.bestbuy.testsuite;
+
+import com.bestbuy.bestbuyinfo.ProductSteps;
+import com.bestbuy.testbase.TestBase;
+import com.bestbuy.utils.TestUtils;
+import io.restassured.response.ValidatableResponse;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Title;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.HashMap;
+
+import static org.hamcrest.Matchers.hasValue;
+
+@RunWith(SerenityRunner.class)
+public class ProductCURDWithSteps extends TestBase {
+
+    static String name = "Kuracell - AAA Batteries (4-Pack)" + TestUtils.getRandomValue();
+    static String type = "HardGood" + TestUtils.getRandomValue();
+    static Double price = 5.49;
+    static int shipping = 0;
+    static String upc = "041333424019";
+    static String description = "Compatible with select electronic devices; AAA size; DURALOCK Power Preserve technology; 4-pack";
+    static String manufacturer = "Duracell";
+    static String model = "MN2400B4Z";
+    static String url = "http://www.bestbuy.com/site/duracell-aaa-batteries-4-pack/43900.p?id=1051384074145&skuId=43900&cmp=RMXCC";
+    static String image = "http://img.bbystatic.com/BestBuy_US/images/products/4390/43900_sa.jpg";
+
+    static int productID;
+    @Steps
+    ProductSteps productSteps;
+
+    @Title("This will create new product")
+    @Test
+    public void test001() {
+        ValidatableResponse response = productSteps.createProduct(name, type, price, shipping, upc, description, manufacturer, model, url, image);
+        response.log().all().statusCode(201);
+        productID = response.log().all().extract().path("id");
+
+    }
+
+    @Title("Verify if the product was added to application")
+    @Test
+    public void test002() {
+
+
+        HashMap<String, Object> productMap = productSteps.getProductInfoByProductName(productID);
+        Assert.assertThat(productMap, hasValue(name));
+
+    }
+
+    @Title("This will update product")
+    @Test
+    public void test003() {
+        name = name + "_Updated";
+
+        productSteps.updateProduct(productID, name, type, price, shipping, upc, description, manufacturer, model, url, image);
+
+        HashMap<String, Object> productMap = productSteps.getProductInfoByProductName(productID);
+        Assert.assertThat(productMap, hasValue(name));
+    }
+
+    @Title("This will delete product")
+    @Test
+    public void test004() {
+
+        productSteps.deleteProductByID(productID).statusCode(200);
+        productSteps.getProductById(productID).statusCode(404);
+    }
+}
